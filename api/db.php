9695ea0840cb7632f00e68455831c6d8b826a98f
zxzxzxyo100-sudo <zxzxzxyo100@gmail.com>
2026-06-15 11:25:35 +0200

    Update db.php

--- api/db.php
+++ api/db.php
@@ -20,7 +20,7 @@ function crm_pdo(): PDO
     $port = getenv('DB_PORT') ?: '3306';
     $name = getenv('DB_NAME') ?: 'u495355717_ZIDON';
     $user = getenv('DB_USER') ?: 'u495355717_zxzxzxyol00';
-    $pass = getenv('DB_PASS') ?: 'ziDONA11';
+    $pass = getenv('DB_PASS') ?: 'Zidona11$';
 
     $dsn = "mysql:host={$host};port={$port};dbname={$name};charset=utf8mb4";
 
